91c20b03ab6c201066a38c7af1196d6d9bcb35ff
Andres Mejia <eamejia783@gmail.com>
2017-04-25 17:16:52 -0500

    Inicia la aplicacion patrimonio georeferenciado de ipiales

---
+++ src/app/parallax/parallax.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+@Component({
+  selector: 'app-parallax',
+  templateUrl: './parallax.component.html',
+  styleUrls: ['./parallax.component.css']
+})
+export class ParallaxComponent implements OnInit {
+
+  ngOnInit() {
+  $(document).ready(function () {
+    $('.parallax').parallax();
+  });
+}
+
+}
